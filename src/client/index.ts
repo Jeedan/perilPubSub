07bf22de591ee52d0f803f58e6fd0a0ea7eac3c9
Jeedan <Jeedan.Cuneyt@gmail.com>
2026-04-15 16:49:58 +0300

    added client side transient queues

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,8 +1,30 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
-  console.log("Starting Peril client...");
+	const rabbitConnUrl = "amqp://guest:guest@localhost:5672/";
+	const conn = await amqp.connect(rabbitConnUrl);
+	console.log("Connection successful");
+	console.log("Starting Peril client...");
+	process.on("SIGINT", () => {
+		console.log("Shutting down...");
+		conn.close();
+	});
+
+	const userInput = await clientWelcome();
+
+	await declareAndBind(
+		conn,
+		ExchangePerilDirect,
+		`pause.${userInput}`,
+		PauseKey,
+		SimpleQueueType.Transient,
+	);
 }
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
-  process.exit(1);
+	console.error("Fatal error:", err);
+	process.exit(1);
 });
--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,3 +1,4 @@
+import amqp from "amqplib";
 import type { ConfirmChannel } from "amqplib";
 
 export async function publishJSON<T>(
@@ -10,4 +11,29 @@ export async function publishJSON<T>(
 	ch.publish(exchange, routingKey, JSONbytes, {
 		contentType: "application/json",
 	});
+}
+
+export enum SimpleQueueType {
+	Durable,
+	Transient,
+}
+
+export async function declareAndBind(
+	conn: amqp.ChannelModel,
+	exchange: string,
+	queueName: string,
+	routingKey: string,
+	queueType: SimpleQueueType,
+): Promise<[amqp.Channel, amqp.Replies.AssertQueue]> {
+	const channel = await conn.createChannel();
+	const isDurable = queueType === SimpleQueueType.Durable;
+	const isTransient = queueType === SimpleQueueType.Transient;
+
+	const queue = await channel.assertQueue(queueName, {
+		durable: isDurable,
+		autoDelete: isTransient,
+		exclusive: isTransient,
+	});
+	await channel.bindQueue(queueName, exchange, routingKey);
+	return [channel, queue];
 }
