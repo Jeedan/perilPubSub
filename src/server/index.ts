c638dfbb35c657f89db62804835ae7db7d882aaa
Jeedan <Jeedan.Cuneyt@gmail.com>
2026-04-15 15:05:16 +0300

    installed rabbitmq via docker

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,17 @@
+import amqp from "amqplib";
+
 async function main() {
-  console.log("Starting Peril server...");
+	const rabbitConnUrl = "amqp://guest:guest@localhost:5672/";
+	const conn = await amqp.connect(rabbitConnUrl);
+	console.log("Connection successful");
+	console.log("Starting Peril server...");
+	process.on("SIGINT", () => {
+		console.log("Shutting down...");
+		conn.close();
+	});
 }
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
-  process.exit(1);
+	console.error("Fatal error:", err);
+	process.exit(1);
 });
