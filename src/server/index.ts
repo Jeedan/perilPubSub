52671f9f3e05e000bd29824114a814e6d59fd6e1
Jeedan <Jeedan.Cuneyt@gmail.com>
2026-04-16 15:09:58 +0300

    feat: added server REPL with pause, resume, exit commands

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
-import amqp from "amqplib";
+import amqp, { type ConfirmChannel } from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
 
 async function main() {
 	const rabbitConnUrl = "amqp://guest:guest@localhost:5672/";
@@ -13,15 +14,27 @@ async function main() {
 	});
 
 	const channel = await conn.createConfirmChannel();
-	try {
-		await publishJSON(channel, ExchangePerilDirect, PauseKey, {
-			isPaused: true,
-		});
-	} catch (err: unknown) {
-		if (err instanceof Error) {
-			console.error(err.message);
+
+	printServerHelp();
+	while (true) {
+		const input = await getInput();
+		if (input.length === 0) continue;
+		const command = input[0];
+		if (command === "pause") {
+			console.log("Sending a pause message");
+			await sendMessageToQueue(channel, ExchangePerilDirect, PauseKey, {
+				isPaused: true,
+			});
+		} else if (command === "resume") {
+			console.log("Sending a resume message");
+			await sendMessageToQueue(channel, ExchangePerilDirect, PauseKey, {
+				isPaused: false,
+			});
+		} else if (command === "quit") {
+			console.log("exiting");
+			break;
 		} else {
-			console.error("Something went wrong", err);
+			console.log("Invalid command");
 		}
 	}
 }
@@ -30,3 +43,20 @@ main().catch((err) => {
 	console.error("Fatal error:", err);
 	process.exit(1);
 });
+
+async function sendMessageToQueue<T>(
+	ch: ConfirmChannel,
+	exchange: string,
+	routingKey: string,
+	value: T,
+) {
+	try {
+		await publishJSON(ch, exchange, routingKey, value);
+	} catch (err: unknown) {
+		if (err instanceof Error) {
+			console.error(err.message);
+		} else {
+			console.error("Something went wrong", err);
+		}
+	}
+}
